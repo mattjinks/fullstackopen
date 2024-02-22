df1473ff4c17ce7eda06e4d5a85f34223e4058aa
Matthew <mattjinks@gmail.com>
2024-02-22 17:38:03 -0500

    first commit

---
+++ part1/a_Basics of React/part1/src/App.jsx
@@ -0,0 +1,20 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya"/>
+      <Hello name="Matt"/>
+    </div>
+  )
+}
+
+export default App

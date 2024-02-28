3d163e544beee7ecb5376cc89fd5b4de5867637f
Matthew <mattjinks@gmail.com>
2024-02-27 21:01:44 -0500

    refactor service

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import axios from 'axios'
-
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '4048894994' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    console.log('Get Persons from db')
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+  //console.log('render', persons.length, 'persons')
+
   const handleSearch = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setFilter(event.target.value)
   }
 
@@ -32,26 +40,23 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
+    console.log(`Post ${person.name} to db`)
     if(persons.some(obj => obj.name === person.name)) {
       alert(`${person.name} is already added to phonebook`)
     } else {
-      axios
-      .post('http://localhost:3001/persons', person)
-      .then(response => {
-        console.log(response)
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
-
+      personService
+        .create(person)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
-  const personsToShow = filter.length
+  const personsToShow = filter.length > 0
     ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
     : persons
-
   return (
     <div>
       <h2>Phonebook</h2>
@@ -65,7 +70,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      {personsToShow.map(person => 
+      {personsToShow && personsToShow.map(person => 
         <Person 
           key={person.name} 
           name={person.name}

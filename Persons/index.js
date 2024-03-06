9f6608175541da58942e264155d4d4f1bc55d910
Matthew <mattjinks@gmail.com>
2024-03-06 11:31:35 -0500

    update api

--- Persons/index.js
+++ Persons/index.js
@@ -24,6 +24,28 @@ app.get('/api/persons', (request, response) => {
     response.send(persons)
 })
 
+app.get('/info', (request, response) => {
+    let currentTime = new Date()
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${currentTime}</p>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        response.send(
+            `<h2>${person.name}</h2>
+            <p>${person.number}</p>`
+        )
+    } else {
+        response.status(404).end()
+        console.log(response)
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

ffec6a91e6bd123ca73f52e9df527f7d46e51ba2
Matthew <mattjinks@gmail.com>
2024-03-08 12:17:11 -0500

    Refactor Notes

--- task/src/components/Person.jsx
+++
@@ -1,8 +0,0 @@
-const Person = (props) => {
-    return (
-        <div>
-            {props.name}: {props.number} <button onClick={props.handleDeletePerson}>delete</button>
-        </div>
-    )
-}
-export default Person

38b6937107102237bf943c92f3a636de9236c593
Matthew <mattjinks@gmail.com>
2024-03-12 11:00:17 -0400

    connect to MongoDB

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://mattjinks:${password}@fullstack.q0wceiv.mongodb.net/personApp?retryWrites=true&w=majority&appName=Fullstack`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    id: 1,
+    name: name,
+    number: number
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  //mongoose.connection.close()
+})
+
+Person.find({}).then(result => {
+    result.forEach(person => {
+      let name = person.name
+      let number = person.number
+      console.log(`${name}: ${number}`)
+    })
+    mongoose.connection.close()
+  })
+
+//mongoose.connection.close()
